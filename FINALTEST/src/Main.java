d068461e24fc1a9487a94f3a9d3932bac89da8f3
Carl <carlandrewv@yahoo.com>
2024-05-09 16:31:26 +0800

    test

--- FINALTEST/src/Main.java
+++ FINALTEST/src/Main.java
@@ -1,6 +1,38 @@
-public class Main {
-public Main() {
-        System.out.println("Hello World!");
+import javax.swing.*;
+
+public class Main implements Runnable {
+    JFrame Frame;
+    TestPanel TestPanel;
+    Thread GameThread;
+
+    public Main() {
+        Frame = new JFrame("DATSTRUC_FINALPROJ");
+        Frame.setSize(1920,1080);
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        TestPanel = new TestPanel();
+
+        Frame.setVisible(true);
+        Frame.add(TestPanel);
+
+        start();
+    }
+
+    public void start() {
+        GameThread = new Thread(this);
+        GameThread.start();
+    }
+
+    public void run() {
+        while(true) {
+            TestPanel.moveCars();
+            try {
+                Thread.sleep(1000/60);
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
     public static void main(String[] args) {
         new Main();
