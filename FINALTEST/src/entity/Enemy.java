858a7d7944cb52c5738357ce1caf08d666789616
Randelladie <100122826+Randelladie@users.noreply.github.com>
2024-05-23 22:53:37 +0800

    Made Pursuing

--- FINALTEST/src/entity/Enemy.java
+++ FINALTEST/src/entity/Enemy.java
@@ -19,7 +19,8 @@ public class Enemy extends GameObject {
     @Override
     public void update() {
         position = new Position(position.getX(), position.getY());
-//        Seeking();
+        //Seeking();
+        Pursuing(1300, 800);
     }
 
     @Override
@@ -42,10 +43,83 @@ public class Enemy extends GameObject {
                 if (position.getX() > 100) {
                     position = new Position(position.getX() - 5, position.getY());
                 }
+                else
+                {
+                    position = new Position(500, 500);
+                }
                 if (position.getY() > 100) {
                     position = new Position(position.getX(), position.getY() - 5);
                 }
             }
         }, 0, 1000);
     }
+
+    public void Pursuing(int targetx, int targety) {
+
+        int mx = targetx;
+        int my = targety;
+
+        int speed = 5;
+
+        int tx = position.getX() - Math.abs(mx);
+        int ty = position.getY() - Math.abs(my);
+        float nx = 0f;
+        float ny = 0f;
+        if (tx != 0 && Math.abs(tx) > speed) {
+            if (position.getX() < mx) {
+                nx = (float) tx / (tx + ty);
+            } else {
+                nx = -((float) tx / (tx + ty));
+            }
+        }
+        if (ty != 0 && Math.abs(ty) > speed) {
+            if (position.getY() < my) {
+                ny = (float) ty / (tx + ty);
+            } else {
+                ny = -((float) ty / (tx + ty));
+            }
+        }
+        System.out.println("nx: " + tx + ", ny: " + ty);
+        if (nx>1) { nx = 1; }
+        if (nx<-1) { nx = -1; }
+        if (ny>1) { ny = 1; }
+        if (ny<-1) { ny = -1; }
+        position = new Position((int)(position.getX() + (int)Math.round(nx*(float)speed)), (int)(position.getY() + (int)Math.round(ny*(float)speed)));
+
+        /*
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                int tx = position.getX() - Math.abs(mx);
+                int ty = position.getY() - Math.abs(my);
+                float nx = 0f;
+                float ny = 0f;
+                if (tx != 0) {
+                    if (position.getX() < mx) {
+                        nx = (float) tx / (tx + ty);
+                    } else {
+                        nx = -((float) tx / (tx + ty));
+                    }
+                }
+                if (tx != 0) {
+                    if (position.getY() < my) {
+                        ny = (float) ty / (tx + ty);
+                    } else {
+                        ny = -((float) ty / (tx + ty));
+                    }
+                }
+                System.out.println("nx: " + nx + ", ny: " + ny);
+                if (nx>1) { nx = 1; }
+                if (nx<-1) { nx = -1; }
+                if (ny>1) { ny = 1; }
+                if (ny<-1) { ny = -1; }
+                position = new Position((int)(position.getX() + (nx*2)), (int)(position.getY() + (ny*2)));
+            }
+        }, 0, 1000);
+
+         */
+    }
+
+    //public void
 }
