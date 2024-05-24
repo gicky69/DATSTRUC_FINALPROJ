96d582f4129416efde4fb12e999edd798ab6eb8d
Randelladie <100122826+Randelladie@users.noreply.github.com>
2024-05-24 23:20:50 +0800

    Final Enemy Movement Update
    - No more errors (likely but more testing may be required, please let me know)
    - Instead of using fractional [x/x+y] (this is actually not good, because it moves slower in diagonal). using hypotenuse to create a normalizing vector that is more accurate according to the geometry

--- FINALTEST/src/entity/Enemy.java
+++ FINALTEST/src/entity/Enemy.java
@@ -49,88 +49,51 @@ public class Enemy extends GameObject {
 
     //#region Enemy Actions
 
-    public void MoveTowards(Position pos) {
+    //#region Normalize
 
+    Position Normalize2D(Position pos) { // This normalizes the 2d vector value (search normalize 2d vector to learn more)
         // Gets the target position in x and y
         int px = pos.getX();
         int py = pos.getY();
-        
+
         // Gets the relative target position in x and y
         int tx = position.getX() - Math.abs(px);
         int ty = position.getY() - Math.abs(py);
-        
+
         // Initiates "X velocity" and "Y velocity"
-        float xvel = 0f;
-        float yvel = 0f;
+        float nx = 0f;
+        float ny = 0f;
 
         // This is an equation that gets the normalized vector of target
         if (tx != 0 && Math.abs(tx) > EnemySpeed) {
-            xvel = (float) tx / Math.abs(tx + ty);
+            nx = (float) tx / (float)Math.hypot(Math.abs(tx), Math.abs(ty)); //Math.hypot returns the hypotenuse value from the 2 vectors
         }
         if (ty != 0 && Math.abs(ty) > EnemySpeed) {
-            yvel = (float) ty / Math.abs(tx + ty);
+            ny = (float) ty / (float)Math.hypot(Math.abs(tx), Math.abs(ty));
         }
 
-        // This is to normalize the speed to be more precise
-        // Without this, enemy can sometimes go 10% faster or slower
-        // it checks if nspd is infinite, This is to avoid infinite calculation
-        float nspd = Math.abs(xvel)+Math.abs(yvel);
-        if (xvel!=0 && nspd!=0) { xvel = xvel/nspd; }
-        if (yvel!=0 && nspd!=0) { yvel = yvel/nspd; }
+        return new Position(nx, ny);
+    }
+
+    public void MoveTowards(Position pos) {
+
+        Position normalized = Normalize2D(pos); // Normalizes the relative position so it returns the directional value it is pointing to
+        float xvel = normalized.getfX();
+        float yvel = normalized.getfY();
 
         // Move towards the target with relative velocity times speed
-        if (tx+ty!=0) { // This is to avoid infinite calculation
-            position = new Position(position.getfX() - xvel * (float) EnemySpeed, position.getfY() - yvel * (float) EnemySpeed);
-        } else { //if the number is an exact 45 degrees, do this instead:
-            if (tx>0) { xvel=-0.5f; }
-            if (ty>0) { yvel=-0.5f; }
-            if (tx<0) { xvel=0.5f; }
-            if (ty<0) { yvel=0.5f; }
-            position = new Position(position.getfX() + xvel * (float) EnemySpeed, position.getfY() + yvel * (float) EnemySpeed);
-        }
+        position = new Position(position.getfX() - xvel * (float) EnemySpeed, position.getfY() - yvel * (float) EnemySpeed);
 
     }
 
     public void MoveAwayFrom(Position pos) {
 
-        // Gets the target position in x and y
-        int px = pos.getX();
-        int py = pos.getY();
-
-        // Gets the relative target position in x and y
-        int tx = position.getX() - Math.abs(px);
-        int ty = position.getY() - Math.abs(py);
-
-        // Initiates "X velocity" and "Y velocity"
-        float xvel = 0f;
-        float yvel = 0f;
-
-        // This is an equation that gets the normalized vector of target
-        if (tx != 0 && Math.abs(tx) > EnemySpeed) {
-            xvel = (float) tx / Math.abs(tx + ty);
-        }
-        if (ty != 0 && Math.abs(ty) > EnemySpeed) {
-            yvel = (float) ty / Math.abs(tx + ty);
-        }
-
-        // This is to normalize the speed to be more precise
-        // Without this, enemy can sometimes go 10% faster or slower
-        // it checks if nspd is infinite, This is to avoid infinite calculation
-        float nspd = Math.abs(xvel)+Math.abs(yvel);
-        if (xvel!=0 && Float.isFinite(nspd)) { xvel = xvel/nspd; }
-        if (yvel!=0 && Float.isFinite(nspd)) { yvel = yvel/nspd; }
+        Position normalized = Normalize2D(pos); // Normalizes the relative position so it returns the directional value it is pointing to
+        float xvel = normalized.getfX();
+        float yvel = normalized.getfY();
 
         // Move away from the target with relative velocity times speed
-        if (tx+ty!=0) { // This is to avoid infinite calculation
-            position = new Position(position.getfX() + xvel * (float) EnemySpeed, position.getfY() + yvel * (float) EnemySpeed);
-        } else { //if the number is an exact 45 degrees, do this instead:
-            if (tx>0) { xvel=-0.5f; }
-            if (ty>0) { yvel=-0.5f; }
-            if (tx<0) { xvel=0.5f; }
-            if (ty<0) { yvel=0.5f; }
-            position = new Position(position.getfX() + xvel * (float) EnemySpeed, position.getfY() + yvel * (float) EnemySpeed);
-        }
-
+        position = new Position(position.getfX() + xvel * (float) EnemySpeed, position.getfY() + yvel * (float) EnemySpeed);
     }
 
     //#endregion
